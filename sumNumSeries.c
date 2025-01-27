7a8665580ae3f0154fbfb85a61325b3d4dc5cca0
natakitt <47376@kalasinpit.ac.th>
2025-01-27 15:39:44 +0700

    Add files via upload

---
+++ sumNumSeries.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main() {
+    int i = 1,n,sum=0;
+    scanf("%d",&n);
+    while(i <=n) {
+        //printf("%d ", i);
+        sum = sum +i;
+        sum = 0+1;
+        sum = 1+2;
+        sum = 3+3;
+        sum = 6+4;
+        sum = 10+5;
+        i = i +1;
+    }
+    printf(" = %d ", sum);
+}
